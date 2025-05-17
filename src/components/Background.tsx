068acfa9e8bc0cce4ec95cf7b3420db2e9c26653
Delemangi <milev.stefan@gmail.com>
2025-05-18 00:48:51 +0200

    Update theme

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,18 +1,18 @@
 import { Box, styled } from '@mui/material';
 
-const diagonalLines = `url("data:image/svg+xml;utf8,<svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'><line x1='0' y1='32' x2='32' y2='0' stroke='rgba(255,255,255,0.025)' stroke-width='2'/></svg>")`;
+const squares = `url("data:image/svg+xml;utf8,<svg width='40' height='40' viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='0.35' y='0.35' width='39.3' height='39.3' stroke='rgba(255,255,255,0.018)' stroke-width='0.7' fill='none'/></svg>")`;
 
 const Background = styled(Box)({
   background: `
     linear-gradient(120deg, #232b36 0%, #181e24 100%),
     linear-gradient(100deg, rgba(106,130,251,0.18) 10%, rgba(0,255,208,0.10) 80%),
     linear-gradient(210deg, rgba(255,99,233,0.13) 20%, rgba(0,0,0,0) 80%),
-    ${diagonalLines}
+    ${squares}
   `,
   backgroundBlendMode: 'screen, lighten, lighten, overlay',
   backgroundPosition: 'center, center, center, 0 0',
   backgroundRepeat: 'no-repeat, no-repeat, no-repeat, repeat',
-  backgroundSize: 'cover, cover, cover, 32px 32px',
+  backgroundSize: 'cover, cover, cover, 40px 40px',
   height: '100%',
   left: 0,
   overflowX: 'hidden',
