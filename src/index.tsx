19121fbea10af58480b6fc1942057e54b9a7af78
Delemangi <milev.stefan@gmail.com>
2025-09-14 17:46:43 +0200

    Add easter egg

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,14 @@ import { createRoot } from 'react-dom/client';
 import App from './page/App';
 import ThemeModeProvider from './theme/ThemeModeProvider';
 
+// eslint-disable-next-line no-console
+console.log(
+  '%cHello there! 👋\n%cPoking around? The source lives at %chttps://github.com/Delemangi/homepage',
+  'color:#6a82fb;font-weight:700;font-size:14px',
+  'color:inherit;font-size:12px',
+  'color:#ee3f71;text-decoration:underline;font-size:12px',
+);
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
