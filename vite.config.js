8faf37f6eb97e8661aede0e171e256005556103b
Delemangi <milev.stefan@gmail.com>
2026-03-21 17:07:33 +0100

    Attempt fix for async loading

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const cfAsyncPlugin = () => ({
+  name: 'cf-async',
+  transformIndexHtml(html) {
+    return html.replaceAll(
+      /<script\s+type="module"(?![^>]*data-cfasync)/gu,
+      '<script data-cfasync="false" type="module"',
+    );
+  },
+});
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cfAsyncPlugin()],
 });
