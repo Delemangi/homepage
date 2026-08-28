5d0f5e934e23758f77248639ee1f5873efe767a5
Delemangi <milev.stefan@gmail.com>
2026-08-29 00:25:47 +0200

    refactor(constants): remove unused birthday

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-export const BIRTHDAY = '2001-12-08';
 export const BIRTH_INSTANT = 1_007_766_000_000;
 export const SITE_TITLE = 'Delemangi';
 export const AURORA_ANIMATION = 'auroraSweep 3s ease-in-out infinite alternate';
